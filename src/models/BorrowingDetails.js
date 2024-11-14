4cc336557d584f8de782c70efbeae00e67bafe43
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2024-11-14 20:10:03 +0700

    update UI

--- src/models/BorrowingDetails.js
+++ src/models/BorrowingDetails.js
@@ -2,39 +2,40 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/db.config.js";
 
 const BorrowingDetails = sequelize.define(
-  "BorrowingDetails",
-  {
-    borrow_detail_id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    borrow_id: {
-      type: DataTypes.UUID,
-      references: { model: "borrowings", key: "borrow_id" },
-      onDelete: "CASCADE",
-    },
-    book_id: {
-      type: DataTypes.UUID,
-      references: { model: "books", key: "book_id" },
-      onDelete: "CASCADE",
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1,
-    },
-    return_date: { type: DataTypes.DATEONLY },
-    notes: { type: DataTypes.TEXT },
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: "borrowed", // Giá trị mặc định là 'borrowed'
-    },
-  },
-  {
-    tableName: "borrowing_details",
-    timestamps: false,
-  }
+    "BorrowingDetails", {
+        borrow_detail_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        borrow_id: {
+            type: DataTypes.UUID,
+            references: { model: "borrowings", key: "borrow_id" },
+            onDelete: "CASCADE",
+        },
+        book_id: {
+            type: DataTypes.UUID,
+            references: { model: "books", key: "book_id" },
+            onDelete: "CASCADE",
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+        },
+        return_date: { type: DataTypes.DATEONLY },
+        notes: {
+            type: DataTypes.TEXT,
+            default: "Không có",
+        },
+        status: {
+            type: DataTypes.STRING,
+            defaultValue: "borrowed", // Giá trị mặc định là 'borrowed'
+        },
+    }, {
+        tableName: "borrowing_details",
+        timestamps: false,
+    }
 );
 
 export default BorrowingDetails;
